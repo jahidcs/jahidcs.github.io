a2a9924deca6a11a77f2316e6bb027524166586f
jahidcs <csq.jahid@gmail.com>
2024-07-25 21:50:55 +0600

    1st trial production ready design

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -4,6 +4,7 @@ fetch("/assets/json/data.json")
     document.getElementById("name").textContent = data.name;
     document.getElementById("profession").textContent = data.profession;
     document.getElementById("bio").textContent = data.bio;
+    document.getElementById("contact-email").textContent = data.contact[0];
   })
   .catch((error) => {
     console.error("Error fetching data:", error);
