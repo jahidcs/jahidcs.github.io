52e78f6dfe07d7300402598f05149ed66517e4ec
jahidcs <csq.jahid@gmail.com>
2024-07-26 20:39:34 +0600

    experience section added

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -9,3 +9,46 @@ fetch("/assets/json/data.json")
   .catch((error) => {
     console.error("Error fetching data:", error);
   });
+
+const companiesList = document.querySelector(".companies");
+const detailsContainer = document.querySelector(".details");
+
+fetch("/assets/json/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data.experience.forEach((company, index) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = company.organization;
+      listItem.dataset.companyIndex = index;
+      companiesList.appendChild(listItem);
+
+      listItem.addEventListener("click", () => {
+        // Remove the 'selected' class from all list items
+        const selectedItem = document.querySelector(".companies li.selected");
+        if (selectedItem) {
+          selectedItem.classList.remove("selected");
+        }
+
+        // Add the 'selected' class to the clicked list item
+        listItem.classList.add("selected");
+
+        const companyIndex = listItem.dataset.companyIndex;
+        const companyDetails = data.experience[companyIndex];
+
+        const formattedDetail = companyDetails.detail.replace(/\n/g, "<br>");
+        detailsContainer.innerHTML = `
+            <h3>${companyDetails.organization}</h3>
+            <h4>${companyDetails.position}</h4>
+            <p class='period'>${companyDetails.start} - ${companyDetails.end}</p>
+            <p class='exp-details'>${formattedDetail}</p>
+          `;
+      });
+    });
+
+    // Select the first company as default
+    const firstCompany = companiesList.querySelector("li");
+    firstCompany.click();
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
